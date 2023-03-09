41b55a62331db0f9188557eec204a78e0237eefb
linh nguyen <linh.duy.ng@gmail.com>
2023-03-09 09:50:04 +0700

    hilt setup

--- app/src/main/java/com/ndta/apptracker/MainActivity.kt
+++ app/src/main/java/com/ndta/apptracker/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.ndta.apptracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.ndta.apptracker.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e("TAG", "RUNDAO: ${runDAO.hashCode()}")
     }
 }
---
+++ app/src/main/java/com/ndta/apptracker/di/AppModule.kt
@@ -0,0 +1,29 @@
+package com.ndta.apptracker.di
+
+import android.content.Context
+import androidx.room.Room
+import com.ndta.apptracker.db.Constants.RUNNING_DATABASE_NAME
+import com.ndta.apptracker.db.RunningDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(@ApplicationContext app: Context) = Room.databaseBuilder(
+        app,
+        RunningDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+}
